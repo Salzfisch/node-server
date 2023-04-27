18df175cccc2e7eecfaadf30ea9ac377f47c9af6
Salzfisch <1114844107@qq.com>
2023-04-28 00:38:45 +0800

    add

---
+++ sample/js/b.js
@@ -0,0 +1,7 @@
+//AJAX
+// var xhr = new XMLHttpRequest();
+// xhr.open('GET', '/getWeather?city=beijing', true);
+// xhr.send();
+// xhr.onload = function(){
+//     console.log(JSON.parse(xhr.responseText));
+// }
